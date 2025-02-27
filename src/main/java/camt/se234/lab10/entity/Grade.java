f6aa7ad69dc634faba217d06d402319860c2161c
Titipon Tawong <titipon_tawong@cmu.ac.th>
2025-02-28 02:29:57 +0700

    back to default

--- src/main/java/camt/se234/lab10/entity/Grade.java
+++ src/main/java/camt/se234/lab10/entity/Grade.java
@@ -3,18 +3,8 @@ package camt.se234.lab10.entity;
 import lombok.Builder;
 import lombok.Data;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 @Data
 @Builder
 public class Grade {
-    private static final Logger logger = LoggerFactory.getLogger(Grade.class);
-    String grade;  // Vulnerable to log-based information leakage
-
-    public void logGrade() {
-        // Logging sensitive information without encryption or masking
-        logger.info("Grade: " + grade);
-    }
+    String grade;
 }
-
