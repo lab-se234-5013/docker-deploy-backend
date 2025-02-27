23e4a94ba61014d32b60c65a587bec54ecfb16a9
Titipon Tawong <titipon_tawong@cmu.ac.th>
2025-02-28 02:28:21 +0700

    add some code

--- src/main/java/camt/se234/lab10/entity/Grade.java
+++ src/main/java/camt/se234/lab10/entity/Grade.java
@@ -3,8 +3,18 @@ package camt.se234.lab10.entity;
 import lombok.Builder;
 import lombok.Data;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Data
 @Builder
 public class Grade {
-    String grade;  // No authorization check to verify if user has permission to set the grade
+    private static final Logger logger = LoggerFactory.getLogger(Grade.class);
+    String grade;  // Vulnerable to log-based information leakage
+
+    public void logGrade() {
+        // Logging sensitive information without encryption or masking
+        logger.info("Grade: " + grade);
+    }
 }
+
